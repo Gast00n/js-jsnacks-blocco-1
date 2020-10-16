b33b97f3896c3dec02b81d290a47966a9e6aed64
Marco Giulietti <ga6t00n@gmail.com>
2020-10-16 13:26:48 +0200

    Sncak 2 - Fatto

--- js/main.js
+++ js/main.js
@@ -1,41 +1,55 @@
 /**
  * 
- *  SNACK 1
+ *  SNACK 1 (Decommenta per attivare il codice)
  * 
  */
 
-var num1 = parseInt( prompt('Inserisci il primo numero') );
-var num2 = parseInt( prompt('Inserisci il secondo numero') );
+// var num1 = parseInt( prompt('Inserisci il primo numero') );
+// var num2 = parseInt( prompt('Inserisci il secondo numero') );
 
-if (num1 > num2) {
-    document.getElementById('snack1').innerHTML = 'Il numero maggiore è: ' + num1; 
-} else if (num1 < num2) {
-    document.getElementById('snack1').innerHTML = 'Il numero maggiore è: ' + num2; 
+// if (num1 > num2) {
+//     document.getElementById('snack1').innerHTML = 'Il numero maggiore è: ' + num1; 
+// } else if (num1 < num2) {
+//     document.getElementById('snack1').innerHTML = 'Il numero maggiore è: ' + num2; 
+// } else {
+//     document.getElementById('snack1').innerHTML = 'I due numeri sono uguali'; 
+// }
+
+ /**
+ * 
+ *  SNACK 2 (Decommenta per attivare il codice)
+ * 
+ */
+
+var parola1 = prompt('Inserisci la prima parola');
+var long1 = parola1.length;
+
+var parola2 = prompt('Inserisci la seconda parola');
+var long2 = parola2.length;
+
+if (long1 > long2) {
+    document.getElementById('snack2').innerHTML = parola2 + ' ' + parola1;
+} else if (long2 > long1) {
+    document.getElementById('snack2').innerHTML = parola1 + ' ' + parola2;
 } else {
-    document.getElementById('snack1').innerHTML = 'I due numeri sono uguali'; 
+    document.getElementById('snack2').innerHTML = 'Le due parole sono lunghe uguali';
 }
 
  /**
  * 
- *  SNACK 2
+ *  SNACK 3 (Decommenta per attivare il codice)
  * 
  */
 
  /**
  * 
- *  SNACK 3
+ *  SNACK 4 (Decommenta per attivare il codice)
  * 
  */
 
  /**
  * 
- *  SNACK 4
- * 
- */
-
- /**
- * 
- *  SNACK 5
+ *  SNACK 5 (Decommenta per attivare il codice)
  * 
  */
 
