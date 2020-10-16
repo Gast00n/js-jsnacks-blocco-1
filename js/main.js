39bc2d0a3fe3c46b68faf92c25fdbb7b683ac8bd
Marco Giulietti <ga6t00n@gmail.com>
2020-10-16 13:21:55 +0200

    Snack 1 - Fatto

---
+++ js/main.js
@@ -0,0 +1,42 @@
+/**
+ * 
+ *  SNACK 1
+ * 
+ */
+
+var num1 = parseInt( prompt('Inserisci il primo numero') );
+var num2 = parseInt( prompt('Inserisci il secondo numero') );
+
+if (num1 > num2) {
+    document.getElementById('snack1').innerHTML = 'Il numero maggiore è: ' + num1; 
+} else if (num1 < num2) {
+    document.getElementById('snack1').innerHTML = 'Il numero maggiore è: ' + num2; 
+} else {
+    document.getElementById('snack1').innerHTML = 'I due numeri sono uguali'; 
+}
+
+ /**
+ * 
+ *  SNACK 2
+ * 
+ */
+
+ /**
+ * 
+ *  SNACK 3
+ * 
+ */
+
+ /**
+ * 
+ *  SNACK 4
+ * 
+ */
+
+ /**
+ * 
+ *  SNACK 5
+ * 
+ */
+
+ 
